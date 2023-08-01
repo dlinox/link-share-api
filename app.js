c5deda001d9439fb68824752b9fa96cf3de8425c
Karol A. Valencia J <71881340+Karol71290@users.noreply.github.com>
2023-08-01 21:06:04 +0200

    Update
    Updated app.js and initiated node.js package

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+const PORT = 3000; // You can change the port if needed
 
-app.get('/', (req, res)  => {
-    res.send('Hello')
-})
+// Temporary storage for shared links
+let links = [];
 
-app.listen(3000);
+app.use(cors());
+app.use(bodyParser.json());
+
+// Route to get all shared links
+app.get('/links', (req, res) => {
+  res.json(links);
+});
+
+// Route to share a new link
+app.post('/links', (req, res) => {
+  const { url, title, description } = req.body;
+  const newLink = { url, title, description, votes: 0 };
+  links.push(newLink);
+  res.status(201).json(newLink);
+});
+
+// Route to delete a link
+app.delete('/links/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = links.findIndex(link => link.id === id);
+  if (index !== -1) {
+    links.splice(index, 1);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Route to vote for a link
+app.post('/links/:id/vote', (req, res) => {
+  const id = parseInt(req.params.id);
+  const link = links.find(link => link.id === id);
+  if (link) {
+    link.votes += 1;
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
