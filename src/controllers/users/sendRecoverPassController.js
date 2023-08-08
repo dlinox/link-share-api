4bcc33c5cc0a1a662ecae302dcbc512f24a0887a
Evelin Arboleda <evelinarboleda23@gmail.com>
2023-08-08 12:47:37 +0200

    add recovery pass code and send mail

--- src/controllers/users/sendRecoverPassController.js
+++ src/controllers/users/sendRecoverPassController.js
@@ -1,30 +1,16 @@
 const randomstring = require('randomstring');
 
-const getDb = require('../../db/getDb');
-const { invalidCredentialsError } = require('../../services/errorService');
+const { updateRecoveryPassCodeModel } = require('../../models/users');
 const sendMailService = require('../../services/sendEmailService');
-const validateSchemaService = require('../../services/validateSchemaService');
-const { recoverPassSchema } = require('../../schema/users');
 
 const sendRecoverPassController =  async(req, res, next) => {
-    let connection;
+    
     try {
-        connection = await getDb();
-        let { email } = req.body;
-        await validateSchemaService(recoverPassSchema, { email }, next);
         
-        const [users] = await connection.query(
-            `SELECT email FROM users WHERE email = ?`,
-            [email]
-        );
-
-        email = users[0].email;
-
-        if (!email) {
-            invalidCredentialsError();
-        }
         const recoverPassCode = randomstring.generate(10);
+        const { email } = req.body;
 
+        await updateRecoveryPassCodeModel(email, recoverPassCode, next);
         sendEmail(email, recoverPassCode, next);
 
         res.status(201).json({
@@ -32,14 +18,10 @@ const sendRecoverPassController =  async(req, res, next) => {
             messages:'We have sent you an email. Please check your inbox.'
         });
     } catch (err) {
-        
-        await connection.rollback();
-
         next(err); 
-    } finally {
-        if (connection) connection.release();
-    }
+    } 
 }
+
 
 const sendEmail = async(email, recoverPassCode, next) => {
     // We create the subject of the password recovery email.
