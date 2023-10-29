af228b3518a0dbafcbcf3c005e8235d8a9687952
Lino Puma Ticona <nearlino20@gmail.com>
2023-10-29 15:21:20 -0500

    con preview

--- src/controllers/links/getLinkPreviewController.js
+++ src/controllers/links/getLinkPreviewController.js
@@ -1,32 +1,25 @@
-// const linkPreviewGenerator = require('link-preview-generator');
+const linkPreviewGenerator = require('link-preview-generator');
 
 const getLinkPreviewController = async (req, res, next) => {
     try {
         const { url } = req.query;
 
         // Obtén la previsualización del enlace
-        // const resPreview = await linkPreviewGenerator(url);
+        const resPreview = await linkPreviewGenerator(url);
 
         let dataResponse = {
             status: 'ok',
-            data: {
-                title: 'Heroicons',
-                description:
-                    'Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.',
-                domain: 'heroicons.com',
-                img: 'https://heroicons.com/_next/static/media/social-card.fefc68e0.jpg',
-                favicon: 'https://heroicons.com/favicon.ico',
-            },
-            message: 'success',
+            data: null,
+            message: null,
         };
 
-        // if (resPreview.status === 'error') {
-        //     dataResponse.status = 'error';
-        //     dataResponse.message = resPreview.message;
-        // } else {
-        //     dataResponse.message = 'success';
-        //     dataResponse.data = resPreview;
-        // }
+        if (resPreview.status === 'error') {
+            dataResponse.status = 'error';
+            dataResponse.message = resPreview.message;
+        } else {
+            dataResponse.message = 'success';
+            dataResponse.data = resPreview;
+        }
 
         res.json(dataResponse);
     } catch (err) {
